43feb2933da09e4057635eb3b55e415d0389e06a
kopalych <kopalychmail@gmail.com>
2026-04-06 17:56:24 +0200

    Task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const formElement = document.querySelector('form.login-form');
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
 
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) alert('All form fields must be filled in');
+
+  const data = { email: email.trim(), password: password.trim() };
+  console.log(data);
+
+  form.reset();
+});
