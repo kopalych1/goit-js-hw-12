bc5f6c874b1760d6fbd841b442d47cc9173e46d3
kopalych <kopalychmail@gmail.com>
2026-04-06 19:19:24 +0200

    Task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonElement = document.querySelector('button.change-color');
+const spanElement = document.querySelector('span.color');
+
+buttonElement.addEventListener('click', event => {
+  const randomColor = getRandomHexColor();
+
+  document.querySelector('body').style['background-color'] = randomColor;
+  spanElement.textContent = randomColor;
+});
