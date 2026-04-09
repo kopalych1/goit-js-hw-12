581aa91e88d946e077161b518a918cfd682c885f
kopalych <kopalychmail@gmail.com>
2026-04-09 20:34:47 +0200

    Task 1: fix: correct counting of categories items

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 const categories = document.querySelector('ul#categories');
 
-console.log('Number of categories:', categories.children.length);
+console.log('Number of categories:', categories.querySelectorAll('li.item').length);
 
 categories.querySelectorAll('li.item').forEach(element => {
   console.log('Category:', element.querySelector('h2').innerText);
