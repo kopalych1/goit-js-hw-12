3428f7032dd013e1da467bede2329709b8b33d54
kopalych <kopalychmail@gmail.com>
2026-04-09 21:01:57 +0200

    Task 4: fix: missing return and trim logic

--- js/task-4.js
+++ js/task-4.js
@@ -3,12 +3,15 @@ formElement.addEventListener('submit', event => {
   event.preventDefault();
 
   const form = event.target;
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-  if (!email || !password) alert('All form fields must be filled in');
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+    return;
+  }
 
-  const data = { email: email.trim(), password: password.trim() };
+  const data = { email: email, password: password };
   console.log(data);
 
   form.reset();
